483b728772c83819865c02735d4f94dbdb8e7814
Sigit Prasetyo <sgt.prassz@gmail.com>
2024-01-05 01:52:43 +0700

    update otp

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-try {
-  new (require('./lib/functions.js'))().run();
+(async function(){
   process.on('uncaugtException', console.log)
+  try { 
+  await new (require('./lib/functions.js'))().run();
 } catch (e) {
   console.log({
     Error: e, path: __dirname
   })
 }
+})()
+
+// try {
+//   new (require('./lib/functions.js'))().run();
+//   process.on('uncaugtException', console.log)
+// } catch (e) {
+//   console.log({
+//     Error: e, path: __dirname
+//   })
+// }
